3b0015475d57022fd1a61f8fb152bdd9f7a8e9d4
Kayz-mann <kayodefredrickbalogun@gmail.com>
2022-08-15 19:49:21 +0100

    add: fetch cars from API create order using API

--- src/components/HomeMap/index.tsx
+++ src/components/HomeMap/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, Image, FlatList } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { DrawerActions } from '@react-navigation/native';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listCars } from '../../graphql/queries';
 
-import cars from '../../../assets/data/cars';
+// import cars from '../../../assets/data/cars';
 import StackHeader from '../StackHeader';
+import { ListCarsQuery } from '../../API';
 
 
 
-const HomeMap: React.FC = ({navigation}: any) => {
+const HomeMap: React.FC = ({ navigation }: any) => {
+  const [cars, setCars] = useState<any>();
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const response = (await API.graphql(
+          graphqlOperation(
+            listCars
+          )
+        )) as {
+          data: ListCarsQuery; errors: any[]
+          }
+        setCars(response.data.listCars)
+      } catch(e: any) {
+        
+      }
+    }
+  })
   const getImage = (type: string) => {
     if (type === 'UberX') {
         return require('../../../assets/images/top-UberX.png')
@@ -31,7 +51,7 @@ const HomeMap: React.FC = ({navigation}: any) => {
         longitudeDelta: 0.0121
       }}
     >
-    {cars.map((item) => (
+    {/* {cars.map((item: any) => (
       <Marker
         key={item.id}
         coordinate={{ latitude: item.latitude, longitude: item.longitude }}
@@ -48,8 +68,8 @@ const HomeMap: React.FC = ({navigation}: any) => {
           source={getImage(item.type)}
         />
       </Marker>
-    ))}
-    {/* <FlatList
+    ))} */}
+    <FlatList
       data={cars}
       keyExtractor={(item: any) => item.id.toString()}
       renderItem={({ item }) => (
@@ -57,12 +77,19 @@ const HomeMap: React.FC = ({navigation}: any) => {
         coordinate={{ latitude: item.latitude, longitude: item.longitude }}
       >
         <Image
-          style={{ width: 70, height: 70, resizeMode: 'contain' }}
+          style={{
+            width: 70,
+            height: 70,
+            resizeMode: 'contain',
+            transform: [{
+              rotate: `${item.heading}deg`
+            }]
+          }}
           source={getImage(item.type)}
         />
       </Marker>
       )}
-    /> */}
+    />
     </MapView>
 </View>
     )
